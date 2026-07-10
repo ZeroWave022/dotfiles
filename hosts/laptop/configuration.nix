98f6e3ab31ec65be38fb010361388a23bda71b8d
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2026-07-10 16:03:28 +0200

    feat: detect printers automatically

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -68,7 +68,22 @@
   console.keyMap = "no";
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+      brlaser
+    ];
+  };
+
+  services.ipp-usb.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
