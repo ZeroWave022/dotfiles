c69fc22d07ab30418613e99cbf310395f573e62c
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2026-07-17 17:18:17 +0200

    feat: add docker

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -16,5 +16,6 @@
     ./programs
     ./secrets
     ./users
+    ./virtualisation
   ];
 }
--- modules/system/users/default.nix
+++ modules/system/users/default.nix
@@ -7,6 +7,7 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "docker"
     ];
     shell = pkgs.zsh;
   };
