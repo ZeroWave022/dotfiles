4c168388731bfb0b78773270b16d664ff3a1d703
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2026-08-04 13:42:33 +0200

    docs: add note about "virtualisation"

--- modules/system/virtualisation/default.nix
+++ modules/system/virtualisation/default.nix
@@ -1,5 +1,8 @@
 { ... }:
 
 {
+  # virtualization > virtualisation
+  # Keeping the British English spelling for relevant files and folders
+  # until NixOS supports the American English spelling, if ever.
   virtualisation.docker.enable = true;
 }
