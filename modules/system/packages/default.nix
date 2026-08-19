2536484604c010198e317f18c8bef52582c4cd53
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2026-08-19 17:30:39 +0200

    feat: add jdk and maven

--- modules/system/packages/default.nix
+++ modules/system/packages/default.nix
@@ -3,10 +3,12 @@
 {
   environment.systemPackages = with pkgs; [
     # Development
+    maven
     nixfmt
     nodejs_24
     pnpm
     python314
+    temurin-bin-17
     wezterm
 
     # Utilities
